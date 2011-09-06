94ef2dc8fe529253c394ae36cea4fa8398f8d03f
Davi Ferreira <contato@daviferreira.com>
2011-09-06 11:55:38 -0300

    Basic structure for the examples done.

--- 20110906_testando_jasmine_parte1/exemplo/tests/spec/suites/boasvindas-spec.js
+++ 20110906_testando_jasmine_parte1/exemplo/tests/spec/suites/boasvindas-spec.js
@@ -1,21 +1,40 @@
 describe('Exibição da mensagem de boas-vindas', function(){
   beforeEach(function(){
     setFixtures('<div id="mensagem" />');
-    this.data_hora = '';
   });
 
-  it("Deve exibir 'Bom-dia!' entre 5 da manhã e meio-dia", function(){
+  it("Deve exibir 'Bom-dia!' entre 5:00 e 11:59", function(){
+    boasvindas('05:00');
+    expect($('#mensagem').text()).toEqual('Bom-dia!');
   });
 
-  it("Deve exibir 'Boa-tarde!' entre meio-dia e 6 da tarde", function(){
+  it("Deve exibir 'Boa-tarde!' entre 12:00 e 17:59", function(){
+    boasvindas('12:00');
+    expect($('#mensagem').text()).toEqual('Boa-tarde!');
   });
 
-  it("Deve exibir 'Boa-noite!' entre 6 da tarde e meia-noite", function(){
+  it("Deve exibir 'Boa-noite!' entre 18:00 e 23:59", function(){
+    boasvindas('18:00');
+    expect($('#mensagem').text()).toEqual('Boa-noite!');
   });
 
-  it("Deve exibir 'Dormir é para os fracos!' entre meia-noite e 6 da manhã", function(){
+  it("Deve exibir 'Dormir é para os fracos!' de 00:00 a 04:59", function(){
+    boasvindas('00:00');
+    expect($('#mensagem').text()).toEqual('Dormir é para os fracos!');
   });
 
-  it("Deve exibir a mensagem correta de acordo com a hora do usuário", function(){
+  it("Deve exibir, por padrão, a mensagem de acordo com a hora do cliente", function(){
+    var data = new Date;
+    data.setTime(data.getTime()); 
+    var hora = data.getHours();
+    boasvindas();
+    if(hora >= 5 && hora < 12)
+      expect($('#mensagem').text()).toEqual('Bom-dia!');
+    else if(hora >= 12 && hora < 18)
+      expect($('#mensagem').text()).toEqual('Boa-tarde!');
+    else if(hora >= 18 && hora <= 23)
+      expect($('#mensagem').text()).toEqual('Boa-noite!');
+    else
+      expect($('#mensagem').text()).toEqual('Dormir é para os fracos!');
   });
 });
