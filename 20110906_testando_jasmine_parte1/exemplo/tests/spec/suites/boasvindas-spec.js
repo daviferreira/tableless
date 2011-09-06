3d35975d89b8eeee3b36c70861ffa4bd85e72a94
Davi Ferreira <contato@daviferreira.com>
2011-09-06 10:49:12 -0300

    First testing code.

---
+++ 20110906_testando_jasmine_parte1/exemplo/tests/spec/suites/boasvindas-spec.js
@@ -0,0 +1,21 @@
+describe('Exibição da mensagem de boas-vindas', function(){
+  beforeEach(function(){
+    setFixtures('<div id="mensagem" />');
+    this.data_hora = '';
+  });
+
+  it("Deve exibir 'Bom-dia!' entre 5 da manhã e meio-dia", function(){
+  });
+
+  it("Deve exibir 'Boa-tarde!' entre meio-dia e 6 da tarde", function(){
+  });
+
+  it("Deve exibir 'Boa-noite!' entre 6 da tarde e meia-noite", function(){
+  });
+
+  it("Deve exibir 'Dormir é para os fracos!' entre meia-noite e 6 da manhã", function(){
+  });
+
+  it("Deve exibir a mensagem correta de acordo com a hora do usuário", function(){
+  });
+});
