b7c983d10994fb5ad6ef621e4481eb27239badf6
Davi Ferreira <contato@daviferreira.com>
2011-09-06 12:41:45 -0300

    Refactoring and references.

--- 20110906_testando_jasmine_parte1/exemplo/tableless.js
+++ 20110906_testando_jasmine_parte1/exemplo/tableless.js
@@ -1,20 +1,21 @@
 function boasvindas(hora_atual){
   var hora;
+
   if(typeof hora_atual == 'undefined'){
     var data = new Date;
     data.setTime(data.getTime()); 
     hora = data.getHours();
   }else{
     hora = hora_atual.split(':');
-    hora = parseInt(hora[0]);
+    hora = parseInt(hora[0].replace(/^0/, ''));
   }
 
-  if(hora >= 5 && hora < 12)
-    $('#mensagem').text('Bom-dia!');
-  else if(hora >= 12 && hora < 18)
-    $('#mensagem').text('Boa-tarde!');
-  else if(hora >= 18 && hora <= 23)
-    $('#mensagem').text('Boa-noite!');
-  else
+  if(hora < 5) 
     $('#mensagem').text('Dormir é para os fracos!');
+  else if(hora < 12)
+    $('#mensagem').text('Bom-dia!');
+  else if(hora < 18)
+    $('#mensagem').text('Boa-tarde!');
+  else
+    $('#mensagem').text('Boa-noite!');
 }
--- 20110906_testando_jasmine_parte1/exemplo/tests/spec/suites/boasvindas-spec.js
+++ 20110906_testando_jasmine_parte1/exemplo/tests/spec/suites/boasvindas-spec.js
@@ -1,40 +1,55 @@
 describe('Exibição da mensagem de boas-vindas', function(){
   beforeEach(function(){
     setFixtures('<div id="mensagem" />');
+    this.horas = [];
   });
 
   it("Deve exibir 'Bom-dia!' entre 5:00 e 11:59", function(){
-    boasvindas('05:00');
-    expect($('#mensagem').text()).toEqual('Bom-dia!');
+    this.horas = ['05:00', '09:33', '10:22', '11:59'];
+    for(i in this.horas){
+      boasvindas(this.horas[i]);
+      expect($('#mensagem').text()).toEqual('Bom-dia!');
+    }
   });
 
   it("Deve exibir 'Boa-tarde!' entre 12:00 e 17:59", function(){
-    boasvindas('12:00');
-    expect($('#mensagem').text()).toEqual('Boa-tarde!');
+    this.horas = ['12:00', '13:33', '16:00', '17:59'];
+    for(i in this.horas){
+      boasvindas(this.horas[i]);
+      expect($('#mensagem').text()).toEqual('Boa-tarde!');
+    }
   });
 
   it("Deve exibir 'Boa-noite!' entre 18:00 e 23:59", function(){
-    boasvindas('18:00');
-    expect($('#mensagem').text()).toEqual('Boa-noite!');
+    this.horas = ['18:00', '19:56', '22:43', '23:59'];
+    for(i in this.horas){
+      boasvindas(this.horas[i]);
+      expect($('#mensagem').text()).toEqual('Boa-noite!');
+    }
   });
 
   it("Deve exibir 'Dormir é para os fracos!' de 00:00 a 04:59", function(){
-    boasvindas('00:00');
-    expect($('#mensagem').text()).toEqual('Dormir é para os fracos!');
+    this.horas = ['00:00', '01:01', '02:48', '04:59'];
+    for(i in this.horas){
+      boasvindas(this.horas[i]);
+      expect($('#mensagem').text()).toEqual('Dormir é para os fracos!');
+    }
   });
 
   it("Deve exibir, por padrão, a mensagem de acordo com a hora do cliente", function(){
     var data = new Date;
     data.setTime(data.getTime()); 
     var hora = data.getHours();
+
     boasvindas();
-    if(hora >= 5 && hora < 12)
-      expect($('#mensagem').text()).toEqual('Bom-dia!');
-    else if(hora >= 12 && hora < 18)
-      expect($('#mensagem').text()).toEqual('Boa-tarde!');
-    else if(hora >= 18 && hora <= 23)
-      expect($('#mensagem').text()).toEqual('Boa-noite!');
-    else
+
+    if(hora < 5)
       expect($('#mensagem').text()).toEqual('Dormir é para os fracos!');
+    else if(hora < 12)
+      expect($('#mensagem').text()).toEqual('Bom-dia!');
+    else if(hora < 18)
+      expect($('#mensagem').text()).toEqual('Boa-tarde!');
+    else
+      expect($('#mensagem').text()).toEqual('Boa-noite!');
   });
 });
